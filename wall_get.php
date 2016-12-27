a317a44a2ce1952b951736a7cc805e0bb0120c6c
Daniil Marynich <bandydan@gmail.com>
2016-12-27 22:03:51 +0200

    insert is working

--- wall_get.php
+++ wall_get.php
@@ -1,6 +1,5 @@
 <?php
 
-// echo phpinfo();exit;
 include_once "const.php";
 
 if (isset($_POST['group_id']) && ! empty($_POST['group_id'])) {
@@ -16,7 +15,6 @@ if (isset($_POST['group_id']) && ! empty($_POST['group_id'])) {
         'sex',
     ];
 
-
     $data =[
         'owner_id' => -1 * $_POST['group_id'],//for groups sign '-' is added
         'sort' => 'id_asc',
@@ -30,21 +28,14 @@ if (isset($_POST['group_id']) && ! empty($_POST['group_id'])) {
     $url = METHOD_URI . 'wall.get?' .
         http_build_query($data);
 
-    $json = file_get_contents($url);
+    // $json = file_get_contents($url);
 
-    file_put_contents("test_data.json", $json);
+    // file_put_contents("test_data.json", $json);
 
-    // exit();
     $result = json_decode(file_get_contents($url), true);
-    // echo "<pre>";
-    // var_dump($result);
-    // echo "</pre>";
-    // exit;
 
+    // $result =json_decode(file_get_contents("test_data.json"), true);
 
-    $result =json_decode(file_get_contents("test_data.json"), true);
-
-    // var_dump($result);exit;
     $photo_src = [];
 
     foreach ($result['response'] as $key => $value) {
@@ -75,7 +66,7 @@ if (isset($_POST['group_id']) && ! empty($_POST['group_id'])) {
     save_photo($photo_src);
 
 
-        foreach ($photo_src as $key => $value) {
-            print "<img src=\"$value\"><br><p>$key</p>";
-        }
+        // foreach ($photo_src as $key => $value) {
+            // print "<img src=\"$value\"><br><p>$key</p>";
+        // }
 }
